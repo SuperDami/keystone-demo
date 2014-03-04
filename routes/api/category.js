6aadf8419e38b13b3cfb9945288bbcc3d4d66175
superdami <chen.zhejun@rakuraku-inc.com>
2014-03-04 19:50:00 +0900

    Update api

--- routes/api/category.js
+++ routes/api/category.js
@@ -1,39 +1,89 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {	
+	// var locals = res.locals;
+	// locals.filters = {
+	// 	category: req.params.category,
+	// 	listType: req.params.listType
+	// };
+	// locals.data = {
+	// 	posts: [],
+	// 	categories: []
+	// };
+
+	// console.log("req.params.category " + req.params.category);
+	// console.log("req.params.type " + req.params.type);
+	// console.log("req.params.pageNumber " + req.params.pageNumber);
+
+	// keystone.list('PostCategory').model.findOne({ name: locals.filters.category }).exec(function(err, result) {
+	// 	locals.data.category = result;
+	// 	var q = keystone.list('Post').paginate({
+	// 	page: req.query.page || req.params.pageNumber,
+	// 		perPage: 10,
+	// 		maxPages: 10
+	// 	})
+	// 	.where('state', 'published')
+	// 	.sort('-publishedDate')
+	// 	.populate('author categories');
+
+	// 	if (locals.data.category) {
+	// 		q.where('categories').in([locals.data.category]);
+	// 		if (locals.filters.listType == 'header') {
+	// 			q.where('showHeader', 'Yes')
+	// 		}
+	// 		q.exec(function(err, results) {
+	// 			res.json(results);
+	// 		});
+	// 	}
+	// });
+
 	var locals = res.locals;
 	locals.filters = {
 		category: req.params.category,
-		listType: req.params.listType
 	};
 	locals.data = {
 		posts: [],
 		categories: []
 	};
 
-	console.log("req.params.category " + req.params.category);
-	console.log("req.params.type " + req.params.type);
-	console.log("req.params.pageNumber " + req.params.pageNumber);
+	console.log("params.category " + locals.filters.category);
+	console.log("req.query.page " + req.query.page);
 
 	keystone.list('PostCategory').model.findOne({ name: locals.filters.category }).exec(function(err, result) {
 		locals.data.category = result;
-		var q = keystone.list('Post').paginate({
-		page: req.query.page || req.params.pageNumber,
-			perPage: 10,
-			maxPages: 10
-		})
-		.where('state', 'published')
-		.sort('-publishedDate')
-		.populate('author categories');
 
 		if (locals.data.category) {
-			q.where('categories').in([locals.data.category]);
-			if (locals.filters.listType == 'header') {
-				q.where('showHeader', 'Yes')
-			}
-			q.exec(function(err, results) {
-				res.json(results);
-			});
-		}
+			var categoryData = new Object();
+			var allPost = keystone.list('Post')
+			.paginate({
+				page: req.query.page || 1,
+				perPage: 10,
+				maxPages: 10
+			})
+			.where('state', 'published')
+			.sort('-publishedDate')
+			.populate('author categories')
+			.where('categories').in([locals.data.category]);
+
+			var headerPost = keystone.list('Post')
+			.paginate({
+				page: 1,
+				perPage: 10,
+				maxPages: 10
+			})
+			.where('state', 'published')
+			.sort('-publishedDate')
+			.populate('author categories')
+			.where('categories').in([locals.data.category])
+			.where('showHeader', 'Yes');
+
+			allPost.exec(function(err, results){
+				categoryData['list'] = results;
+				headerPost.exec(function(err, results){
+					categoryData['header'] = results;
+					res.json(categoryData);
+				});
+			})
+		}		
 	});
 }
--- routes/api/gallery.js
+++ routes/api/gallery.js
@@ -1,7 +1,22 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-	keystone.list('Gallery').model.where('state', 'published').exec(function(err, result){
-		res.json(result)
+	// keystone.list('Gallery').model.where('state', 'published').exec(function(err, result){
+	// 	res.json(result)
+	// });		
+
+	var galleryData = new Object();
+	keystone.list('Gallery')			
+	.paginate({
+		page: req.query.page || 1,
+		perPage: 10,
+		maxPages: 10
+	})
+	.where('state', 'published')
+	.sort('-publishedDate')
+	.populate('author categories')
+	.exec(function(err, result){
+		galleryData['header'] = result
+		res.json(galleryData);
 	});		
 }
