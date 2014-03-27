c2d4f12e3aa47f4d1b700d49cb332b74cea6e186
superdami <chen.zhejun@rakuraku-inc.com>
2014-03-27 11:44:25 +0900

    No need to remove space and enter

--- routes/api/intro.js
+++ routes/api/intro.js
@@ -6,8 +6,7 @@ exports = module.exports = function(req, res) {
 		keystone.list('Intro').model.find().sort("-publishedDate").exec(function(err, result) {
 			console.log('result ' + result)
 			var string = result[0]['json'];
-			var resultString = string.replace(/\r\n|\n\r|\r\r|\n\n|\s+/g, '');
-			var json = JSON.parse(resultString);
+			var json = JSON.parse(string);
 			res.json(json);
 		});
 	}
